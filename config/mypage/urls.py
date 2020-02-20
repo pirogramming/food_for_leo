943531032aef6388262282f74dd1dad48bf2146e
kang <rkdrud34@gmail.com>
2020-02-20 22:32:36 +0900

    강경욱 실험

--- config/mypage/urls.py
+++ config/mypage/urls.py
@@ -8,7 +8,7 @@ app_name = 'mypage'
 urlpatterns = [
     path('profile/', update_profile, name='profile'),
     path('create_pet/', create_pet, name='create_pet'),
-    path('pet_info/', pet_info, name='pet_info'),
+
     path('email_update/', email_update, name='email_update'),
     path('tel_update/', tel_update, name='tel_update'),
 
