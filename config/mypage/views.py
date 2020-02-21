71113c106b8b7f1eaf7fcaa9c9b9dca74ae7e12a
DaeHwan Kim <mystletain@naver.com>
2020-02-21 23:26:41 +0900

    Pet Detail Added

--- config/mypage/views.py
+++ config/mypage/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db import transaction
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
@@ -141,7 +142,19 @@ def pet_delete(request, pk):
 
 @login_required
 def diary_list(request):
-    return render(request, 'mypage/diary_list.html')
+    diaries = Diary.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(diaries, 3)
+    try:
+        diarys = paginator.page(page)
+    except PageNotAnInteger:
+        diarys = paginator.page(1)
+    except EmptyPage:
+        diarys = paginator.page(paginator.num_pages)
+
+    return render(request, 'mypage/diary_list.html', {
+        'diarys':diarys,
+    })
 
 
 @login_required
